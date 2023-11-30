c9683a49c4300a7b4adf61e707360e2a4d15c499
Edwardropde <edwardropha@gmail.com>
2023-11-30 17:31:38 +0000

    corrections

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -2,4 +2,4 @@
 # a Bash script that sends a request to a
 # URL passed as an argument, and displays
 # only the status code of the response
-curl -s -o /dev/null -w "%{http_code}" "$1"
+curl -s -w "%{http_code}" "$1" -o /dev/null
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a POST request to
 # the passed URL, and displays the body of the response.
-curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
+curl -s -X "POST" "$1" -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
